e1ee8b2e4b2e07618fa91f4153c02b4afd3f70f7
zunishkhan <113103720+zunishkhan@users.noreply.github.com>
2022-12-06 07:48:45 -0800

    Add files via upload

---
+++ index3.php
@@ -0,0 +1,28 @@
+<?php
+    // $language = "C++";
+    // switch($language){
+    //     case "C++":
+    //         echo "Your Favourite Language is C++";
+    //         break;
+    //     case "PHP-8":
+    //         echo "Your Favourite Language is PHP-8";
+    //         break;
+    //         case "Javascript":
+    //             echo "Your Favourite Language is Javascript";
+    //             break;
+    //         case "xml":
+    //             echo "Your Favourite Language is xml";
+    //             break;
+    //         default:
+    //         echo "Your lavourite language is neither C++,PHP-8,Javascript nor xml ";
+    // } 
+
+    $n = 1;
+    while($n <= 10){
+        echo("the number is $n <br/>");
+        $n++;
+    }
+   ?>
+
+
+
