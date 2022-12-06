1b21570a355257a3a628e73981b62172c31cf773
zunishkhan <113103720+zunishkhan@users.noreply.github.com>
2022-12-06 07:41:04 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,122 +1,18 @@
 <?php
-  
-//   // Cast float to int
-//   $x = 23465.768;
-//   $int_cast = (int)$x;
-//   echo $int_cast;
-  
-//   echo "<br>";
-  
-//   // Cast string to int
-//   $x = "23465.768";
-//   $int_cast = (int)$x;
-//   echo $int_cast;
-//   
-// echo(pi());
-//    echo(min(0, 150, 50, 30, 20, -8, -200));
-
-
-//    echo(max(0, 150, 50, 30, 20, -8, -200));
-
-// echo(abs(-6.7));
-
-// echo"<br>";
-// echo(sqrt(64));
-
-// echo(rand(10, 100));
-
-// define("GREETING", "Welcome to W3Schools.com!");
-// echo GREETING;
-
-
-// $x = 6;
-
-// function test(){
- 
-//  echo"<p>variable x inside function is: $x </p>";
-
- 
-// }
-// test();
-//  function static_variable(){
-
-//   static $x = 10;
-//   $y = 20;
-
-//   $x++;
-
-//   $y++;
-  
-//   echo "static: " .$x . "</br>";
-//   echo "non_static: " .$y . "</br>";
-
-
-
-//  }
-
-// function add(){
-//     $num1 = 10;
-//     $num2 = 20;
-//     $result = $num1+$num2;
-//     return $result;
-
-// }
-//       echo("The answer to your addition is : " . add());
-
-// function minus(){
-//      $num1 = 25;
-//     $num2 = 20;
-//     $result = $num1-$num2;
-//      return $result;
-
-//  }
-//         echo("The answer to your substraction is : " . minus());
-
-
-
-// function multiply(){
-//      $num1 = 25;
-//     $num2 = 20;
-//     $result = $num1*$num2;
-//      return $result;
-
-// }
-//        echo("The answer to your multiplication is : " . multiply());
-
-//  function divide(){
-//       $num1 = 5;
-//     $num2 = 20;
-//     $result = $num1/$num2;
-//      return $result;
-
-//  }
-//         echo("The answer to your division is : " . divide());
-
-// function subtract($num1,$num2){
-
-//     $res = $num1-$num2;
-//     return $res;
-// }
-
-// echo("the answer to your subtraction is:" . subtract(43,23));
-
-// $x1 = 1024;
-// var_dump(is_int($x1));
-// echo "The given num $x1 is in integer </br>";
-
-// $x2 = 99.84;
-// var_dump(is_int($x2));
-
-// echo "The given num $x2 is in not integer </br>";
-
-$x1 = 40;
-$y1 = 10;
-function mytest(){
-    global $x1 , $y1;
-    $y1 = $x1*$y1;
-}
-mytest();
-echo $y1;
-
-
-?>
+    $language = "C++";
+    switch($language){
+        case "C++":
+            echo "Your Favourite Language is C++";
+            break;
+        case "PHP-8":
+            echo "Your Favourite Language is PHP-8";
+            break;
+            case "Javascript":
+                echo "Your Favourite Language is Javascript";
+                break;
+            case "xml":
+                echo "Your Favourite Language is xml";
+                break;
+            default:
+            echo "Your lavourite language is neither C++,PHP-8,Javascript nor xml ";
+    }
