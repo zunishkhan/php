1cac6d80857cedeeb9be1bc6ace05c42c268a2d3
zunishkhan <113103720+zunishkhan@users.noreply.github.com>
2022-12-20 07:51:51 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,18 +1,79 @@
 <?php
-    $language = "C++";
-    switch($language){
-        case "C++":
-            echo "Your Favourite Language is C++";
-            break;
-        case "PHP-8":
-            echo "Your Favourite Language is PHP-8";
-            break;
-            case "Javascript":
-                echo "Your Favourite Language is Javascript";
-                break;
-            case "xml":
-                echo "Your Favourite Language is xml";
-                break;
-            default:
-            echo "Your lavourite language is neither C++,PHP-8,Javascript nor xml ";
-    }
+include('config.php');
+
+$fetch = "SELECT * FROM `php_crud`";
+$result = $conn->query($fetch);
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>php_crud</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+      
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php crud</title>
+</head>
+<body>
+    <div class="container">
+        <h1>PHP CRUD </h1>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">S:no</th>
+                    <th scope="col">user_name</th>
+                    <th scope="col">user_email</th>
+                    <th scope="col">user_password</th>
+                    <th scope="col">action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                ?><tr>
+                            <td><?php  echo $row['id'];  ?></td>
+                            <td><?php echo $row['name'];  ?></td>
+                            <td><?php echo $row['email'];  ?></td>
+                            <td><?php echo $row['password'];  ?></td>
+                            <td>
+                                  
+                            
+<a href="update.php?id=<?php  echo $row['user_id'] ?>" class="btn btn-success">Edit</a>
+                        |
+<a href="delete.php?id=<?php  echo $row['user_id']   ?>" class="btn btn-danger">Delete</a>
+                            </td>
+                    </tr>
+                <?php
+
+                    }
+                }
+                ?>
+
+
+
+            </tbody>
+
+        </table>
+
+    </div>
+</body>
+</html>
